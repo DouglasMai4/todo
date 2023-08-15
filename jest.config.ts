0b9ac091f2ccb7a6067aa0e86d96251c0a38ea74
Douglas Maia <douglasmai4@protonmail.com>
2023-08-15 17:43:01 -0300

    feat: Tests implemented

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+        'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+  
