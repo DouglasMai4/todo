8115d9ea8fae889d742dc93793e523d3d460fb9f
Douglas Maia <douglasmai4@protonmail.com>
2023-08-16 15:19:55 -0300

    feat: Modal added to add a task

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { store } from '../store';
+
+import Modal from './components/Modal.vue';
 import Input from './components/Input.vue';
 </script>
 
@@ -7,15 +10,58 @@ import Input from './components/Input.vue';
     <section>
       <h1>Tarefas</h1>
 
-      <form>
-        <Input
-          type="text"
-          placeholder="Título da tarefa"
-        />
-      </form>
+      <hr />
+
+      <h2
+        v-if="!store.todos.length"
+      >
+        Nenhuma tarefa adicionada
+      </h2>
     </section>
   </main>
+
+  <Modal>
+    <h1>Adicionar Tarefa</h1>
+    <form @submit="handleSubmit">
+      <Input
+        type="text"
+        placeholder="Título da tarefa"
+        v-model="title"
+      />
+
+      <span>{{ msg }}</span>
+
+      <button type="submit">
+        Adicionar
+      </button>
+    </form>
+  </Modal>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      title: '',
+      msg: ''
+    };
+  },
+  methods: {
+    handleSubmit(e: Event) {
+      e.preventDefault();
+
+      if (!this.title) {
+        this.msg = 'O título não pode ficar vazio';
+        return;
+      }
+
+      store.add(this.title);
+      this.title = '';
+      this.msg = 'Tarefa adicionada';
+    }
+  }
+};
+</script>
 
 <style scoped lang="scss">
 @import './assets/colors.scss';
@@ -31,6 +77,8 @@ main {
     border-radius: 1rem;
     background-color: $bg-color-s;
     border: 1px solid $c-primary;
+    margin: 1rem;
+    text-align: center;
     box-shadow: 5px 5px 15px rgb(0 0 0 / 50%);
   }
 }
---
+++ src/components/Modal.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Icon } from '@iconify/vue';
+
+const open = ref(false);
+</script>
+
+<template>
+  <button
+    class="open-btn"
+    @click="open = true"
+  >
+    <Icon icon="fa6-solid:plus" />
+  </button>
+
+  <teleport to="body">
+    <dialog v-if="open">
+      <div class="content">
+        <slot />
+
+        <button
+          class="close-btn"
+          @click="open = false"
+        >
+          <Icon icon="fa6-solid:xmark" />
+        </button>
+      </div>
+    </dialog>
+  </teleport>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/colors.scss';
+
+.open-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: $bg-color;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  aspect-ratio: 1 / 1;
+  width: 3rem;
+  font-size: 2rem;
+  background-color: $c-secondary;
+  border: none;
+  border-radius: 1rem;
+  box-shadow: 0 0 15px rgb(0 0 0 / 50%);
+  cursor: pointer;
+  transition: background 300ms ease;
+
+  &:hover {
+    background-color: $c-primary;
+  }
+}
+
+dialog {
+  position: fixed;
+  width: 100%;
+  height: 100dvh;
+  background-color: transparent;
+  border: none;
+  backdrop-filter: blur(8px);
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .content {
+    position: relative;
+    padding: 1rem;
+    border: 1px solid $c-primary;
+    border-radius: 1rem;
+    background-color: $bg-color;
+    color: $c-text;
+    margin: 1rem;
+    box-shadow: 5px 5px 15px rgb(0 0 0 / 50%);
+
+    .close-btn {
+      position: absolute;
+      top: -1.25rem;
+      right: -1.25rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      aspect-ratio: 1 / 1;
+      width: 2.5rem;
+      font-size: 2rem;
+      border-radius: 50%;
+      border: none;
+      background-color: $c-error;
+      color: $bg-color;
+      cursor: pointer;
+      transition: filter 300ms ease;
+
+      &:hover {
+        filter: brightness(0.85);
+      }
+    }
+  }
+}
+</style>
