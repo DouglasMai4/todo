eb439c6bb9dbde17a3592e5e18a620b59bde9637
Douglas Maia <douglasmai4@protonmail.com>
2023-08-16 14:19:28 -0300

    feat: Initial style

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <section>
+      <h1>Tarefas</h1>
+
+      <form>
+        <Input
+          type="text"
+          placeholder="Título da tarefa"
+        />
+      </form>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+@import './assets/colors.scss';
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100dvh;
+
+  section {
+    padding: 1rem;
+    border-radius: 1rem;
+    background-color: $bg-color-s;
+    border: 1px solid $c-primary;
+    box-shadow: 5px 5px 15px rgb(0 0 0 / 50%);
+  }
 }
 </style>
